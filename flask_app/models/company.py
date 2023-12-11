fddb1e4e3ceb3229335138890c743b5b5c747dd9
Kyle Runge <krunge1@hotmail.com>
2023-12-11 12:50:00 -0600

    Company class and get_all method

--- flask_app/models/company.py
+++ flask_app/models/company.py
@@ -6,6 +6,8 @@ import re
 bcrypt = Bcrypt(app)
 from pprint import pprint
 
+from flask_app import user
+
 class Company:
     db = "crm"
 
@@ -17,20 +19,97 @@ class Company:
         self.zip_code = data['zip_code']
         self.created_at = data['created_at']
         self.updated_at = data['updated_at']
+        self.user_id = data['user_id']
+        self.account_representative = None
+        self.contacts = []
 
     #Create
-    # @classmethod
-    # def create(cls, data):
-    #     if not cls.validate_sighting_data(data):
-    #         pprint(data['number_of_sasquatches'])
-    #         return False
-    #     query = """INSERT INTO sightings (location, what_happened, date_of_sighting, number_of_sasquatches, user_id) 
-    #     VALUES (%(location)s, %(what_happened)s, %(date_of_sighting)s, %(number_of_sasquatches)s, %(user_id)s)
-    #     ;
-    #     """
-    #     result = connectToMySQL(cls.db).query_db(query, data)
-        # return result
+    @classmethod
+    def create(cls, data):
+        if not cls.validate_company_data(data):
+            pprint(data['name'])
+            return False
+        query = """INSERT INTO companies (name, address, city, state, zip_code, user_id) 
+        VALUES (%(name)s, %(address)s, %(city)s, %(state)s, %(zip_code)s, %(user_id)s)
+        ;
+        """
+        result = connectToMySQL(cls.db).query_db(query, data)
+        return result
+
+    @staticmethod # VALIDATE company
+    def validate_company_data(data): 
+        is_valid = True
+        if len(data['name'])<1:
+            flash('Name of company must be provided.')
+            is_valid = False
+        if len(data['address'])<1:
+            flash('Address must be provided.')
+            is_valid = False
+        if len(data['city'])<1:
+            flash('City must be provided.')
+            is_valid = False
+        if len(data['state'])<1:
+            flash('State must be provided.')
+            is_valid = False
+        if len(data['zip_code'])<1:
+            flash('Zip code must be provided.')
+            is_valid = False
+        return is_valid
     #Read
+    @classmethod # Get all companies
+    def get_all_companies(cls):
+        query = """SELECT * from companies
+        LEFT JOIN users
+        ON companies.user_id = users.id
+
+        LEFT JOIN contacts
+        ON companies.id = contacts.company_id
+
+        LEFT JOIN users as users2
+        ON users2.id = contacts.user_id
+        ;
+        """
+        results = connectToMySQL(cls.db).query_db(query)
+        if not results:
+            return []
+        all_companies = []
+        for i in range(len(results)):
+            if results[i]["id"] != results[i-1]["id"] or i == 0:
+                this_company = cls(results[i])
+                all_companies.append(this_company)
+
+            account_rep_data = {
+                    "id": results[i]['users.id'],
+                    "user_name" : results[i]['user_name'],
+                    "first_name": results[i]['first_name'],
+                    "last_name": results[i]['last_name'],
+                    "email": results[i]['email'],
+                    "password": results[i]['password'],
+                    "user_level" : results[i]['user_level'],
+                    "user_status" : results[i]['user_status'],
+                    "created_at": results[i]['users.created_at'],
+                    "updated_at": results[i]['users.updated_at'],
+                    }
+            account_representative = user.User(account_rep_data)
+            this_company.account_representative = account_representative
+            if results[i]['users2.id'] is not None:
+                contact_data = {
+                    "id": results[i]['users2.id'],
+                    "first_name": results[i]['users2.first_name'],
+                    "last_name": results[i]['users2.last_name'],
+                    "email": results[i]['users2.email'],
+                    "phone_number" : results[i]['users2.phone_number'],
+                    "address" : results[i]['users2.address'],
+                    "city" : results[i]['users2.city'],
+                    "zip_code" : results[i]['users2.zip_code'],
+                    "password": results[i]['users2.password'],
+                    "created_at": results[i]['users2.created_at'],
+                    "updated_at": results[i]['users2.updated_at'],
+                    }
+                contact = user.User(contact_data)
+                this_company.contacts.append(contact)
+        return all_companies
+
 
     #Update
     #Delete
